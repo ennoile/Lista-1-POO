fd77158e080a9f2700915881f504644804fb1fc3
Heloíse Brito <heloisecruz2006@gmail.com>
2025-05-25 15:17:50 -0300

    Update Contribuinte.java

--- src/main/java/br/edu/ufam/Questao10/Contribuinte.java
+++ src/main/java/br/edu/ufam/Questao10/Contribuinte.java
@@ -1,1 +1,17 @@
+public abstract class Contribuinte {
+    protected String nome;
+    protected String documento;
 
+    public Contribuinte(String nome, String documento){
+        this.nome = nome;
+        this.documento = documento;
+    }
+
+    public abstract double calcularImposto();
+
+    public void exibirImposto(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Documento(CPF ou CNPJ): " + documento);
+        System.out.printf("Imposto a ser pago: R$%.2f\n", calcularImposto());
+    }
+}
