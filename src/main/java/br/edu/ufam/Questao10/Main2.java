3ba4ac39dba26f49171f25d6e3c163be1b9bd7c4
Heloíse Brito <heloisecruz2006@gmail.com>
2025-05-25 15:21:43 -0300

    Update Main2.java

--- src/main/java/br/edu/ufam/Questao10/Main2.java
+++ src/main/java/br/edu/ufam/Questao10/Main2.java
@@ -1,1 +1,19 @@
+//10) Imposto de Renda - A receita Federal possui um cadastro dos contribuintes. Cada
+//contribuinte possui CPF ou CNPJ e nome. Os contribuintes podem ser (1,0 ponto)
 
+public class Main2 {
+    public static void main (String[] args){
+        Contribuinte[] contribuintes = new Contribuinte[6];
+
+        contribuintes[0] = new PessoaFisica("Paulo", "234.345.678-00",1200);
+        contribuintes[1] = new PessoaFisica("Ricardo", "294.125.648-00",2000);
+        contribuintes[2] = new PessoaFisica("Ana", "224.345.608-00",3223);
+        contribuintes[3] = new PessoaFisica("LTGA", "245.446.629-00",9000);
+        contribuintes[4] = new PessoaFisica("RN-Modas", "274.155.671-00",14000);
+        contribuintes[5] = new PessoaFisica("SAFIRA-AE", "217.675.038-00",5000);
+
+        for (Contribuinte c : contribuintes){
+            c.exibirImposto();
+        }
+    }
+}
