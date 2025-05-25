2760ece662230e849c9ce4ff3781c19afa473802
Heloíse Brito <heloisecruz2006@gmail.com>
2025-05-25 15:20:09 -0300

    Update PessoaJuridica.java

--- src/main/java/br/edu/ufam/Questao10/PessoaJuridica.java
+++ src/main/java/br/edu/ufam/Questao10/PessoaJuridica.java
@@ -1,1 +1,21 @@
+public class PessoaJuridica extends Contribuinte{
+    
+    private double rendaBruta;
 
+    public PessoaJuridica(String nome, String cnpj, double rendaBruta){
+        super(nome, cnpj);
+        this.rendaBruta = rendaBruta;
+    }
+
+    @Override
+    public double calcularImposto(){
+        return rendaBruta * 0.10;
+    }
+
+    @Override
+    public void exibirImposto() {
+        super.exibirImposto();
+        System.out.printf("Renda Bruta: R$ %.2f\n", rendaBruta);
+        System.out.println("---------------------------");
+    }
+}
